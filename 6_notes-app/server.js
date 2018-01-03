0fc0d698cace90546f0080e01271daf2840ae8a3
Sanghyuk <saanghyuk@gmail.com>
2018-01-04 03:58:33 +0900

    6 complete

---
+++ 6_notes-app/server.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const hbs =require('express-handlebars');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+
+const app =express();
+
+//##############HBS SETUP ##############
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname+'/views/partials'
+}));
+app.set('view engine', 'hbs');
+
+
+//CSS
+app.use('/css', express.static(__dirname+'/public/css'));
+const jsonParser = bodyParser.json();
+
+
+//ROUTE
+app.get('/', (req, res)=>{
+   fetch('http://localhost:3004/messages')
+       .then(response=>{
+            response.json().then((json)=>{
+                res.render('home', {
+                    articles: json
+                })
+            })
+                .catch((err)=>{
+                    console.log(err)
+                })
+       })
+});
+
+app.get('/add_note', (req, res)=>{
+    res.render('add_note')
+});
+
+app.get('/edit_note/:id', (req, res)=>{
+    const id=req.params.id;
+    fetch(`http://localhost:3004/messages/${id}`)
+        .then(response=>{
+            response.json().then(json=>{
+                res.render('edit_note',{
+                    articles: json
+                    })
+            })
+        })
+
+});
+
+//POST
+app.post('/api/add_note', jsonParser ,(req, res)=>{
+    fetch('http://localhost:3004/messages', {
+        method: 'POST',
+        body: JSON.stringify(req.body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((response)=>{
+        res.status(200).send();
+    })
+});
+
+//DELETE
+app.delete('/api/delete/:id', (req, res)=>{
+
+    const id=req.params.id;
+    fetch(`http://localhost:3004/messages/${id}`, {
+        method: 'DELETE',
+
+    }).then((response)=>{
+        res.status(200).send()
+    }).catch((e)=>{
+        console.log(e);
+    })
+});
+
+
+//UPDATE PATCH
+app.patch('/api/edit_note/:id', jsonParser,(req, res)=>{
+
+    const id= req.params.id;
+    fetch(`http://localhost:3004/messages/${id}`, {
+        method: 'PATCH',
+        body: JSON.stringify(req.body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((response)=>{
+        res.status(200).send()
+    })
+});
+
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, ()=>{
+   console.log(`Server up on port ${port}`);
+});
+
